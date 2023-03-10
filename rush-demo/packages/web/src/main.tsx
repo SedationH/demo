c4a77e705f2d18da7b656662feff6f2e0bf572ad
SedationH <sedationh@gmail.com>
2023-03-10 19:22:58 +0800

    更换项目的配合方式 直接引用源文件

--- rush-demo/packages/web/src/main.tsx
+++ rush-demo/packages/web/src/main.tsx
@@ -4,7 +4,7 @@ import { sum } from "share-logic"
 import App from "./App"
 import "./index.css"
 
-console.log(sum(1, 2))
+console.log(sum(1, 2, 3))
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
